9a572fa2b205f5c8554b274d025a0c3d49cf5a79
victorgomez2156 <47222250+victorgomez2156@users.noreply.github.com>
2020-03-17 15:45:44 -0400

    asd

---
+++ application/language/english/message_lang.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['title_login_true'] = '¡Log In!';
+$lang['Iniciar_Sesion'] = 'We Are Logging In, Please Wait...';
+
+$lang['title_login_error'] = '¡Data Error!';
+$lang['Error_Datos'] = 'Error in the supplied data. Try again.';
+
+$lang['title_login_blo'] = '¡User blocked!';
+$lang['User_Blo'] = 'This User Is Blocked.';
+
+$lang['title_login_NoBD'] = '¡Unregistered user!';
+$lang['User_NoBD'] = 'This User Is Not Registered.';
+
+$lang['title_login_error_lang'] = '¡Language Error!';
+$lang['Error_Lang'] = 'There was an error loading the language.';
+?>
---
+++ application/language/spanish/message_lang.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['title_login_true'] = '¡Iniciando Sesión!';
+$lang['Iniciar_Sesion'] = 'Estamos Iniciando Su Sesión, Por Favor Espere...';
+
+$lang['title_login_error'] = '¡Error En Datos!';
+$lang['Error_Datos'] = 'El Nombre de Usuario o Contraseña Son Inconrrectos, Verifiquelos y Intente Nuevamente.';
+
+$lang['title_login_blo'] = '¡Usuario Bloqueado!';
+$lang['User_Blo'] = 'Este Usuario Se Encuentra Bloqueado.';
+
+$lang['title_login_NoBD'] = '¡No Registrado!';
+$lang['User_NoBD'] = 'Este Usuario No Se Encuentra Registrado.';
+
+$lang['title_login_error_lang'] = '¡Error de Idioma!';
+$lang['Error_Lang'] = 'Hubo un error al cargar el idioma.';
+
+?>
