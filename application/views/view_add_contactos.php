ff4c9810fcfaf06084fe7098fa475876a7a14ead
crosangel <rosangelcoutinho@hotmail.com>
2020-03-17 19:44:07 +0100

    Update view_add_contactos.php

--- application/views/view_add_contactos.php
+++ application/views/view_add_contactos.php
@@ -218,7 +218,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
      <div class="text-right">
       <div class="credits">
           <!--
-            All the links in the footer should remain intact.
+             the links in the footer should remain intact.
             You can delete the links only if you purchased the pro version.
             Licensing information: https://bootstrapmade.com/license/
             Purchase the pro version form: https://bootstrapmade.com/buy/?theme=NiceAdmin
